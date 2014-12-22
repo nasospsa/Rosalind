19a134fbcde00944c36911296ddb35a373d23476
Nasos Psarrakos <psarrakos@gmail.com>
2014-12-22 22:56:32 +0200

    Refactored cons

--- cons.rb
+++ cons.rb
@@ -1,50 +1,30 @@
 #!/usr/bin/env ruby
 
-
-
-def cons
-	arr = Hash.new { |hash, key| hash[key] = [] }
-	#arr['A'][] = 0
-	#arr['T'][] = 0
-	#arr['C'][] = 0
-	#arr['G'][] = 0
-	contents = File.read("probs/rosalind_cons.txt")
-	lineIdx = -1
-	contents.each_line do |line|
-		lineIdx += 1
-		charIdx = -1
-		line.each_char do |chr|
-			charIdx += 1
-			arr[chr][charIdx] = (arr[chr][charIdx] || 0) + 1
+def cons(words)
+	h = Hash.new { |h, key| h[key] = [] }
+	maxChars = 0
+	words.each do |line|
+		maxChars = line.length if line.length > maxChars
+		line.each_char.with_index do |char, i|
+			h[char][i] = (h[char][i] || 0) + 1
 		end
 	end
 
-	consensus = ''
-	conIdx = 0
-	hashLine = {}
-	table = {'A' => [], 'C' => [], 'G' => [], 'T' => []}
-
-	while conIdx < arr['A'].length do
-		hashLine = {'A' => arr['A'][conIdx]||0,
-					'T' => arr['T'][conIdx]||0,
-					'G' => arr['G'][conIdx]||0,
-					'C' => arr['C'][conIdx]||0}
-		table['A'][conIdx] = arr['A'][conIdx]||0
-		table['C'][conIdx] = arr['C'][conIdx]||0
-		table['G'][conIdx] = arr['G'][conIdx]||0
-		table['T'][conIdx] = arr['T'][conIdx]||0
-		consensus.concat(hashLine.sort{|a,b| a[1] <=> b[1]}.last[0])
-		conIdx += 1
+	consensus = ""
+	for i in 0...maxChars
+		m = h.max_by {|k,v| v[i]||0 }
+		consensus += m[0]
 	end
 	puts consensus
-	
-	table.each do |key, val|
-		str = "#{key}: "
-		puts str.concat(val.join(' '))
+
+	h.sort.each do |k,v|
+		printed = Array.new(maxChars, 0).map.with_index{ |x, i| v[i] || x }.join ' '
+		puts "#{k}: #{printed}"
 	end
-	
-	return arr
+
+	h
 end
 
-cons
+words = File.read("probs/rosalind_cons.txt").tr("\n",'').split(/>Rosalind_\d{4}/)
+cons words
 
